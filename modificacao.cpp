43b31c67a48b68426050bf74c70f493e17d9a0dc
mmlxys <mirellyribeiroaaz@gmail.com>
2024-03-05 11:17:00 -0300

    invadindo git graziane
    add nome no codigo da grazi

---
+++ modificacao.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+int main()
+{
+    string nome = "Grazi";
+    string turma = "Turma34DS";
+    string nome2 = "Mirelly";
+    cout<< nome << endl;
+    cout<< turma << endl;
+    cout<< nome2 << endl;
+
+    return 0;
+}
